e3c7b3f715a34ee7122df343b8303b818753cc5e
Dillan Kerai <sc21dmk@leeds.ac.uk>
2022-07-25 21:09:59 +0100

    passed

---
+++ CodeWars/8kyu/ANeedleInTheHaystack.py
@@ -0,0 +1,6 @@
+def find_needle(haystack):
+    return "found the needle at position {}".format(haystack.index("needle"))
+
+
+print(find_needle(['3', '123124234', None, 'needle', 'world', 'hay', 2, '3', True, False]))
+#  'found the needle at position 3')))
