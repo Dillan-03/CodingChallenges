84da0fdf56b7cff2803ff433733ac9f052b52b2a
Dillan Kerai <sc21dmk@leeds.ac.uk>
2022-07-28 11:40:35 +0100

    passed

---
+++ LeetCode/Easy/9.py
@@ -0,0 +1,11 @@
+from turtle import st
+
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        x = str(x)
+        return (x == x[::-1])
+       
+
+x = Solution() 
+print(x.isPalindrome(-10))
