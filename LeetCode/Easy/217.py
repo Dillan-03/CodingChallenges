488dfe65af92d3169557378008d5161d11154f62
Dillan Kerai <sc21dmk@leeds.ac.uk>
2022-08-16 18:25:35 +0100

    Contains Duplicate

---
+++ LeetCode/Easy/217.py
@@ -0,0 +1,7 @@
+# Contains Duplicate
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        if len(nums) == len(set(nums)):
+            return False
+        else:
+            return True
