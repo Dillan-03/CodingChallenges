15cfd1dd3a990fe7325c72045f6c4cae60615b30
Dillan Kerai <sc21dmk@leeds.ac.uk>
2023-07-05 15:41:34 +0100

    Single Number II

---
+++ LeetCode/Python/Medium/137.py
@@ -0,0 +1,16 @@
+# Single Number II
+from collections import Counter
+
+
+class Solution:
+    def singleNumber(self, nums):
+        holder = Counter(nums)
+        print(holder)
+
+        for key, value in holder.items():
+            if value == 1:
+                return key
+
+
+x = Solution()
+print(x.singleNumber([2, 2, 3, 2]))
